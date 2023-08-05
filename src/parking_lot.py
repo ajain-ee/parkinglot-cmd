5e72cb7980987088751d251151560985c4fc1b98
jain.anuj <jain.anuj@bcg.com>
2023-08-05 14:58:15 +0530

    add few scenarios

--- src/parking_lot.py
+++ src/parking_lot.py
@@ -23,42 +23,43 @@ class ParkingLot:
             slot_number = number + 1
             self.parking_slots.append(ParkingSlot(slot_number))
 
-        print("Created parking lot with {0} slots".format(slots))
+        self.log_message("Created parking lot with {0} slots".format(slots))
 
     def park_on_spot(self, vehicle: Vehicle):
         empty_spots = self.get_vacant_slots()
         if len(empty_spots) == 0:
-            self.log_message()
-            raise RuntimeError("Sorry, parking lot is full")
-        print("Allocated slot number: {0}".format(empty_spots[0].number))
+            self.log_message("Sorry, parking lot is full")
+            return
+        self.log_message("Allocated slot number: {0}".format(empty_spots[0].number))
         self.is_vehicle_already_parked(vehicle)
         empty_spots[0].assign_slot(vehicle)
 
     def is_vehicle_already_parked(self, vehicle: Vehicle):
         for i, slot in enumerate(self.parking_slots):
             if slot.status is ParkingStatuses.PARKED and slot.parked_vehicle.registration_number == vehicle.registration_number:
-                raise RuntimeError(
-                    "Vehicle with registration number {0} is already parked in Slot {1} , cannot reassign the Slot".format(
-                        vehicle.registration_number, slot.number))
+                self.log_message(
+                    "Vehicle with registration number {0} is already parked in Slot {1} , cannot reassign the Slot")
 
     def vacant_from_spot(self, registration_number: str, parking_hours: int):
+        for vehicle in list(map(lambda x: x.parked_vehicle)):
+            
         for i, slot in enumerate(self.parking_slots):
-            if slot.status is not ParkingStatuses.EMPTY and slot.parked_vehicle.registration_number == registration_number:
+            if slot.status is ParkingStatuses.PARKED and slot.parked_vehicle.registration_number == registration_number:
                 slot.vacant_slot()
 
         minimum_parking_charges = 10
         if parking_hours <= 2:
-            print(
+            self.log_message(
                 "Registration Number {0} from Slot {1} has left with Charge {2}".format(registration_number,
                                                                                         slot.number,
                                                                                         minimum_parking_charges))
             return minimum_parking_charges
         else:
             total_charges = minimum_parking_charges + (parking_hours - 2) * 10
-            print(
+            self.log_message(
                 "Registration Number {0} from Slot {1} has left with Charge {2}".format(registration_number,
                                                                                         slot.number,
-                                                                                        minimum_parking_charges))
+                                                                                        total_charges))
             return total_charges
 
     def parking_status(self):
@@ -82,4 +83,3 @@ class ParkingLot:
 
     def log_message(self, message):
         print(message)
-        print("\n")
